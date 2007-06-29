f9fc4bf0d770c03279debab42a5995131bf1ad8f
Liam Girdwood <liam@localhost.localdomain>
2007-06-29 16:33:43 +0100

    Moved Sharp Poodle to ASoC V2 device model.

--- sound/soc/pxa/poodle.c
+++ sound/soc/pxa/poodle.c
@@ -19,11 +19,13 @@
 #include <linux/timer.h>
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
+#include <linux/i2c.h>
 #include <sound/driver.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/soc.h>
 #include <sound/soc-dapm.h>
+#include <sound/initval.h>
 
 #include <asm/mach-types.h>
 #include <asm/hardware/locomo.h>
@@ -46,8 +48,9 @@
 
 static int poodle_jack_func;
 static int poodle_spk_func;
+static struct snd_soc_machine *poodle_mach;
 
-static void poodle_ext_control(struct snd_soc_codec *codec)
+static void poodle_ext_control(struct snd_soc_machine *machine)
 {
 	int spk = 0;
 
@@ -58,55 +61,51 @@ static void poodle_ext_control(struct snd_soc_codec *codec)
 			POODLE_LOCOMO_GPIO_MUTE_L, 1);
 		locomo_gpio_write(&poodle_locomo_device.dev,
 			POODLE_LOCOMO_GPIO_MUTE_R, 1);
-		snd_soc_dapm_set_endpoint(codec, "Headphone Jack", 1);
+		snd_soc_dapm_set_endpoint(machine, "Headphone Jack", 1);
 	} else {
 		locomo_gpio_write(&poodle_locomo_device.dev,
 			POODLE_LOCOMO_GPIO_MUTE_L, 0);
 		locomo_gpio_write(&poodle_locomo_device.dev,
 			POODLE_LOCOMO_GPIO_MUTE_R, 0);
-		snd_soc_dapm_set_endpoint(codec, "Headphone Jack", 0);
+		snd_soc_dapm_set_endpoint(machine, "Headphone Jack", 0);
 	}
 
 	if (poodle_spk_func == POODLE_SPK_ON)
 		spk = 1;
 
 	/* set the enpoints to their new connetion states */
-	snd_soc_dapm_set_endpoint(codec, "Ext Spk", spk);
+	snd_soc_dapm_set_endpoint(machine, "Ext Spk", spk);
 
 	/* signal a DAPM event */
-	snd_soc_dapm_sync_endpoints(codec);
+	snd_soc_dapm_sync_endpoints(machine);
 }
 
 static int poodle_startup(struct snd_pcm_substream *substream)
 {
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_codec *codec = rtd->socdev->codec;
+	struct snd_soc_pcm_link *pcm_link = substream->private_data;
+	struct snd_soc_machine *machine = pcm_link->machine;
 
 	/* check the jack status at stream startup */
-	poodle_ext_control(codec);
+	poodle_ext_control(machine);
 	return 0;
 }
 
 /* we need to unmute the HP at shutdown as the mute burns power on poodle */
-static int poodle_shutdown(struct snd_pcm_substream *substream)
+static void poodle_shutdown(struct snd_pcm_substream *substream)
 {
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_codec *codec = rtd->socdev->codec;
-
 	/* set = unmute headphone */
 	locomo_gpio_write(&poodle_locomo_device.dev,
 		POODLE_LOCOMO_GPIO_MUTE_L, 1);
 	locomo_gpio_write(&poodle_locomo_device.dev,
 		POODLE_LOCOMO_GPIO_MUTE_R, 1);
-	return 0;
 }
 
 static int poodle_hw_params(struct snd_pcm_substream *substream,
 	struct snd_pcm_hw_params *params)
 {
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_codec_dai *codec_dai = rtd->dai->codec_dai;
-	struct snd_soc_cpu_dai *cpu_dai = rtd->dai->cpu_dai;
+	struct snd_soc_pcm_link *pcm_link = substream->private_data;
+	struct snd_soc_dai *cpu_dai = pcm_link->cpu_dai;
+	struct snd_soc_dai *codec_dai = pcm_link->codec_dai;
 	unsigned int clk = 0;
 	int ret = 0;
 
@@ -125,25 +124,25 @@ static int poodle_hw_params(struct snd_pcm_substream *substream,
 	}
 
 	/* set codec DAI configuration */
-	ret = codec_dai->dai_ops.set_fmt(codec_dai, SND_SOC_DAIFMT_I2S |
+	ret = codec_dai->ops->set_fmt(codec_dai, SND_SOC_DAIFMT_I2S |
 		SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBS_CFS);
 	if (ret < 0)
 		return ret;
 
 	/* set cpu DAI configuration */
-	ret = cpu_dai->dai_ops.set_fmt(cpu_dai, SND_SOC_DAIFMT_I2S |
+	ret = cpu_dai->ops->set_fmt(cpu_dai, SND_SOC_DAIFMT_I2S |
 		SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBS_CFS);
 	if (ret < 0)
 		return ret;
 
 	/* set the codec system clock for DAC and ADC */
-	ret = codec_dai->dai_ops.set_sysclk(codec_dai, WM8731_SYSCLK, clk,
+	ret = codec_dai->ops->set_sysclk(codec_dai, WM8731_SYSCLK, clk,
 		SND_SOC_CLOCK_IN);
 	if (ret < 0)
 		return ret;
 
 	/* set the I2S system clock as input (unused) */
-	ret = cpu_dai->dai_ops.set_sysclk(cpu_dai, PXA2XX_I2S_SYSCLK, 0,
+	ret = cpu_dai->ops->set_sysclk(cpu_dai, PXA2XX_I2S_SYSCLK, 0,
 		SND_SOC_CLOCK_IN);
 	if (ret < 0)
 		return ret;
@@ -157,6 +156,16 @@ static struct snd_soc_ops poodle_ops = {
 	.shutdown = poodle_shutdown,
 };
 
+static int poodle_pcm_new(struct snd_soc_pcm_link *pcm_link)
+{
+	pcm_link->audio_ops = &poodle_ops;
+	return snd_soc_pcm_new(pcm_link, 1, 1);
+}
+
+struct snd_soc_pcm_link_ops poodle_pcm = {
+	.new	= poodle_pcm_new,
+};
+
 static int poodle_get_jack(struct snd_kcontrol *kcontrol,
 	struct snd_ctl_elem_value *ucontrol)
 {
@@ -167,13 +176,13 @@ static int poodle_get_jack(struct snd_kcontrol *kcontrol,
 static int poodle_set_jack(struct snd_kcontrol *kcontrol,
 	struct snd_ctl_elem_value *ucontrol)
 {
-	struct snd_soc_codec *codec =  snd_kcontrol_chip(kcontrol);
+	struct snd_soc_machine *machine =  snd_kcontrol_chip(kcontrol);
 
 	if (poodle_jack_func == ucontrol->value.integer.value[0])
 		return 0;
 
 	poodle_jack_func = ucontrol->value.integer.value[0];
-	poodle_ext_control(codec);
+	poodle_ext_control(machine);
 	return 1;
 }
 
@@ -187,13 +196,13 @@ static int poodle_get_spk(struct snd_kcontrol *kcontrol,
 static int poodle_set_spk(struct snd_kcontrol *kcontrol,
 	struct snd_ctl_elem_value *ucontrol)
 {
-	struct snd_soc_codec *codec =  snd_kcontrol_chip(kcontrol);
+	struct snd_soc_machine *machine =  snd_kcontrol_chip(kcontrol);
 
 	if (poodle_spk_func == ucontrol->value.integer.value[0])
 		return 0;
 
 	poodle_spk_func = ucontrol->value.integer.value[0];
-	poodle_ext_control(codec);
+	poodle_ext_control(machine);
 	return 1;
 }
 
@@ -236,7 +245,7 @@ static const struct soc_enum poodle_enum[] = {
 	SOC_ENUM_SINGLE_EXT(2, spk_function),
 };
 
-static const snd_kcontrol_new_t wm8731_poodle_controls[] = {
+static const struct snd_kcontrol_new wm8731_poodle_controls[] = {
 	SOC_ENUM_EXT("Jack Function", poodle_enum[0], poodle_get_jack,
 		poodle_set_jack),
 	SOC_ENUM_EXT("Speaker Function", poodle_enum[1], poodle_get_spk,
@@ -244,73 +253,163 @@ static const snd_kcontrol_new_t wm8731_poodle_controls[] = {
 };
 
 /*
+ * WM8731 2 wire address is determined by GPIO5
+ * state during powerup.
+ *    low  = 0x1a
+ *    high = 0x1b
+ */
+#define WM8731_I2C_ADDR	0x1b
+static unsigned short normal_i2c[] = { WM8731_I2C_ADDR, I2C_CLIENT_END };
+
+/* Magic definition of all other variables and things */
+I2C_CLIENT_INSMOD;
+
+static struct i2c_driver wm8731_i2c_driver;
+static struct i2c_client client_template;
+
+static int poodle_wm8731_write(void *control_data, long data, int size)
+{
+	return i2c_master_send((struct i2c_client*)control_data, 
+		(char*) data, size);
+}
+
+/*
  * Logic for a wm8731 as connected on a Sharp SL-C7x0 Device
  */
-static int poodle_wm8731_init(struct snd_soc_codec *codec)
+static int poodle_mach_probe(struct snd_soc_machine *machine)
 {
-	int i, err;
-
-	snd_soc_dapm_set_endpoint(codec, "LLINEIN", 0);
-	snd_soc_dapm_set_endpoint(codec, "RLINEIN", 0);
-	snd_soc_dapm_set_endpoint(codec, "MICIN", 1);
-
-	/* Add poodle specific controls */
+	struct snd_soc_codec *codec;
+	struct snd_soc_pcm_link *pcm_link;
+	int i, ret;
+	
+	pcm_link = list_first_entry(&machine->active_list, 
+		struct snd_soc_pcm_link, active_list);
+	codec = pcm_link->codec;
+		
+	/* set up poodle codec pins */
+	snd_soc_dapm_set_endpoint(machine, "LLINEIN", 0);
+	snd_soc_dapm_set_endpoint(machine, "RLINEIN", 0);
+	snd_soc_dapm_set_endpoint(machine, "MICIN", 1);
+	
+	/* add poodle specific controls */
 	for (i = 0; i < ARRAY_SIZE(wm8731_poodle_controls); i++) {
-		err = snd_ctl_add(codec->card,
-			snd_soc_cnew(&wm8731_poodle_controls[i],codec, NULL));
-		if (err < 0)
-			return err;
+		if ((ret = snd_ctl_add(machine->card,
+				snd_soc_cnew(&wm8731_poodle_controls[i],
+					machine, NULL))) < 0)
+			return ret;
 	}
 
 	/* Add poodle specific widgets */
-	for (i = 0; i < ARRAY_SIZE(wm8731_dapm_widgets); i++) {
-		snd_soc_dapm_new_control(codec, &wm8731_dapm_widgets[i]);
+	for(i = 0; i < ARRAY_SIZE(wm8731_dapm_widgets); i++) {
+		snd_soc_dapm_new_control(machine, codec, 
+			&wm8731_dapm_widgets[i]);
 	}
 
 	/* Set up poodle specific audio path audio_map */
-	for (i = 0; audio_map[i][0] != NULL; i++) {
-		snd_soc_dapm_connect_input(codec, audio_map[i][0],
+	for(i = 0; audio_map[i][0] != NULL; i++) {
+		snd_soc_dapm_connect_input(machine, audio_map[i][0],
 			audio_map[i][1], audio_map[i][2]);
 	}
-
-	snd_soc_dapm_sync_endpoints(codec);
+	
+	snd_soc_dapm_sync_endpoints(machine);
+	
+	codec->control_data = poodle_mach->private_data;
+	codec->mach_write = poodle_wm8731_write;
+	codec->ops->probe_codec(codec, poodle_mach);
+	
+	/* register card with ALSA upper layers */
+	ret = snd_soc_register_card(poodle_mach);
+	if (ret < 0) {
+		printk(KERN_ERR "%s: failed to register sound card\n",
+			__FUNCTION__);
+		return ret;
+	}
+	
 	return 0;
 }
 
-/* poodle digital audio interface glue - connects codec <--> CPU */
-static struct snd_soc_dai_link poodle_dai = {
-	.name = "WM8731",
-	.stream_name = "WM8731",
-	.cpu_dai = &pxa_i2s_dai,
-	.codec_dai = &wm8731_dai,
-	.init = poodle_wm8731_init,
-	.ops = &poodle_ops,
+struct snd_soc_machine_ops poodle_mach_ops = {
+	.mach_probe = poodle_mach_probe,	
 };
 
-/* poodle audio machine driver */
-static struct snd_soc_machine snd_soc_machine_poodle = {
-	.name = "Poodle",
-	.dai_link = &poodle_dai,
-	.num_links = 1,
-};
-
-/* poodle audio private data */
-static struct wm8731_setup_data poodle_wm8731_setup = {
-	.i2c_address = 0x1b,
-};
-
-/* poodle audio subsystem */
-static struct snd_soc_device poodle_snd_devdata = {
-	.machine = &snd_soc_machine_poodle,
-	.platform = &pxa2xx_soc_platform,
-	.codec_dev = &soc_codec_dev_wm8731,
-	.codec_data = &poodle_wm8731_setup,
-};
-
-static struct platform_device *poodle_snd_device;
-
-static int __init poodle_init(void)
+static int wm8731_i2c_probe(struct i2c_adapter *adap, int addr, int kind)
 {
+	struct i2c_client *i2c;
+	struct snd_soc_pcm_link *hifi;
+	int ret;
+
+	if (addr != WM8731_I2C_ADDR)
+		return -ENODEV;
+
+	client_template.adapter = adap;
+	client_template.addr = addr;
+
+	i2c = kmemdup(&client_template, sizeof(client_template), GFP_KERNEL);
+	if (i2c == NULL)
+		return -ENOMEM;
+	i2c_set_clientdata(i2c, poodle_mach);
+	poodle_mach->private_data = i2c;
+	
+	ret = i2c_attach_client(i2c);
+	if (ret < 0) {
+		printk("failed to attach codec at addr %x\n", addr);
+		goto attach_err;
+	}
+	
+	/* poodle wm8731 hifi interface */
+	hifi = snd_soc_pcm_link_new(poodle_mach, "poodle-hifi", 
+		&poodle_pcm, pxa2xx_pcm, wm8731_codec, wm8731_hifi_dai, 
+		pxa2xx_i2s);
+	if (hifi == NULL) {
+		printk("failed to create HiFi PCM link\n");
+		goto attach_err;
+	}
+	ret =  snd_soc_pcm_link_attach(hifi);
+	if (ret < 0) 
+		goto link_err;
+	
+	return ret;
+
+link_err:
+	snd_soc_machine_free(poodle_mach);
+attach_err:
+	i2c_detach_client(i2c);
+	kfree(i2c);
+	return ret;
+}
+
+static int wm8731_i2c_detach(struct i2c_client *client)
+{
+	snd_soc_machine_free(poodle_mach);
+	i2c_detach_client(client);
+	kfree(client);
+	return 0;
+}
+
+static int wm8731_i2c_attach(struct i2c_adapter *adap)
+{
+	return i2c_probe(adap, &addr_data, wm8731_i2c_probe);
+}
+
+static struct i2c_driver wm8731_i2c_driver = {
+	.driver = {
+		.name = "WM8731 I2C Codec",
+		.owner = THIS_MODULE,
+	},
+	.id =             I2C_DRIVERID_WM8731,
+	.attach_adapter = wm8731_i2c_attach,
+	.detach_client =  wm8731_i2c_detach,
+	.command =        NULL,
+};
+
+static struct i2c_client client_template = {
+	.name =   "WM8731",
+	.driver = &wm8731_i2c_driver,
+};
+
+static int __init poodle_wm8731_probe(struct platform_device *pdev)
+{
+	struct snd_soc_machine *machine;
 	int ret;
 
 	if (!machine_is_poodle())
@@ -324,27 +423,93 @@ static int __init poodle_init(void)
 	locomo_gpio_set_dir(&poodle_locomo_device.dev,
 		POODLE_LOCOMO_GPIO_MUTE_R, 0);
 
-	poodle_snd_device = platform_device_alloc("soc-audio", -1);
-	if (!poodle_snd_device)
+	machine = kzalloc(sizeof(struct snd_soc_machine), GFP_KERNEL);
+	if (machine == NULL)
 		return -ENOMEM;
 
-	platform_set_drvdata(poodle_snd_device, &poodle_snd_devdata);
-	poodle_snd_devdata.dev = &poodle_snd_device->dev;
-	ret = platform_device_add(poodle_snd_device);
+	machine->owner = THIS_MODULE;
+	machine->pdev = pdev;
+	machine->name = "poodle";
+	machine->longname = "wm8731";
+	machine->ops = &poodle_mach_ops;
+	pdev->dev.driver_data = machine;
 
-	if (ret)
-		platform_device_put(poodle_snd_device);
-
+	/* register card */
+	poodle_mach = machine;
+	ret = snd_soc_new_card(machine, 1, SNDRV_DEFAULT_IDX1, 
+		SNDRV_DEFAULT_STR1);
+	if (ret < 0) {
+		printk(KERN_ERR "%s: failed to create pcms\n", __func__);
+		kfree(machine);
+		return ret;
+	}
+	
+	/* register I2C driver for WM8731 codec control */
+	ret = i2c_add_driver(&wm8731_i2c_driver);
+	if (ret < 0) { 
+		printk (KERN_ERR "%s: failed to add i2c driver\n",
+			__FUNCTION__);
+		goto err;
+	}
+	return ret;
+	
+err:
+	kfree(machine);
 	return ret;
 }
 
-static void __exit poodle_exit(void)
+static int __exit poodle_wm8731_remove(struct platform_device *pdev)
 {
-	platform_device_unregister(poodle_snd_device);
+	struct snd_soc_machine *machine = pdev->dev.driver_data;
+	
+	i2c_del_driver(&wm8731_i2c_driver);
+	poodle_mach = NULL;
+	kfree(machine);
+	return 0;
 }
 
-module_init(poodle_init);
-module_exit(poodle_exit);
+#ifdef CONFIG_PM
+static int poodle_wm8731_suspend(struct platform_device *pdev, 
+	pm_message_t state)
+{
+	struct snd_soc_machine *machine = pdev->dev.driver_data;
+	return snd_soc_suspend(machine, state);
+}
+
+static int poodle_wm8731_resume(struct platform_device *pdev)
+{
+	struct snd_soc_machine *machine = pdev->dev.driver_data;
+	return snd_soc_resume(machine);
+}
+
+#else
+#define poodle_wm8731_suspend NULL
+#define poodle_wm8731_resume  NULL
+#endif
+
+static struct platform_driver poodle_wm8731_driver = {
+	.probe		= poodle_wm8731_probe,
+	.remove		= __devexit_p(poodle_wm8731_remove),
+	.suspend	= poodle_wm8731_suspend,
+	.resume		= poodle_wm8731_resume,
+	.driver		= {
+		.name 		= "poodle-wm8731",
+		.owner		= THIS_MODULE,
+	},
+};
+
+static int __init poodle_asoc_init(void)
+{
+	return platform_driver_register(&poodle_wm8731_driver);
+}
+
+static void __exit poodle_asoc_exit(void)
+{
+	platform_driver_unregister(&poodle_wm8731_driver);
+}
+
+module_init(poodle_asoc_init);
+module_exit(poodle_asoc_exit);
 
 /* Module information */
 MODULE_AUTHOR("Richard Purdie");
