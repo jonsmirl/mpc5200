dac723d59b4c329058889761261c1fcbe0c83787
Jon Smirl <jonsmirl@gmail.com>
2009-04-10 16:09:07 -0400

    Refresh of stac9766

---
+++ sound/soc/codecs/stac9766.h
@@ -0,0 +1,17 @@
+/*
+ * stac9766.h  --  STAC9766 Soc Audio driver
+ */
+
+#ifndef _STAC9766_H
+#define _STAC9766_H
+
+#define AC97_STAC_ANALOG_SPECIAL 0x6E
+#define AC97_STAC_STEREO_MIC 0x78
+
+/* STAC9766 DAI ID's */
+#define STAC9766_DAI_AC97_ANALOG		0
+#define STAC9766_DAI_AC97_DIGITAL		1
+
+extern const char stac9766_codec_id[];
+
+#endif
