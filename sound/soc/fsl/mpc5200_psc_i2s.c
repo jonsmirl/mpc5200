ed5c4cf5225f9e014a90560d6ad563eafcbc9903
Jon Smirl <jonsmirl@gmail.com>
2009-04-20 12:14:39 -0400

    Refresh of of-soc-simple-rework

--- sound/soc/fsl/mpc5200_psc_i2s.c
+++ sound/soc/fsl/mpc5200_psc_i2s.c
@@ -877,8 +877,8 @@ static int __devinit psc_i2s_of_probe(struct of_device *op,
 	snd_soc_register_platform(&psc_i2s_pcm_soc_platform);
 
 	/* Tell the ASoC OF helpers about it */
-	of_snd_soc_register_platform(&psc_i2s_pcm_soc_platform, op->node,
-				     &psc_i2s->dai);
+	of_snd_soc_register_platform(&psc_i2s_pcm_soc_platform);
+	of_snd_soc_register_cpu_dai(op->node, &psc_i2s->dai, 1);
 
 	return 0;
 }
