51928752b251da566a35287a9f0c273022912647
Jon Smirl <jonsmirl@gmail.com>
2009-04-20 10:34:49 -0400

    Split out the DMA code from the mpc5200 i2s driver

---
+++ sound/soc/fsl/mpc5200_dma.h
@@ -0,0 +1,20 @@
+/*
+ * Freescale MPC5200 Audio DMA driver
+ */
+
+#ifndef __SOUND_SOC_FSL_MPC5200_DMA_H__
+#define __SOUND_SOC_FSL_MPC5200_DMA_H__
+
+int mpc5200_dma_startup(struct snd_pcm_substream *substream,
+			   struct snd_soc_dai *dai);
+int mpc5200_dma_hw_free(struct snd_pcm_substream *substream,
+			   struct snd_soc_dai *dai);
+void mpc5200_dma_shutdown(struct snd_pcm_substream *substream,
+			     struct snd_soc_dai *dai);
+int mpc5200_dma_trigger(struct snd_pcm_substream *substream, int cmd,
+			   struct snd_soc_dai *dai);
+
+extern const struct snd_pcm_hardware mpc5200_pcm_hardware;
+extern struct snd_soc_platform mpc5200_soc_platform;
+
+#endif /* __SOUND_SOC_FSL_MPC5200_DMA_H__ */
