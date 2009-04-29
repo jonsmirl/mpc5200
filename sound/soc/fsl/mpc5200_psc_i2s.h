df10967f478dab09ee757f2a19e0db01c2d6efe2
Jon Smirl <jonsmirl@gmail.com>
2009-04-28 23:48:04 -0400

    Support internal I2S clock sources on MPC5200
    Support internal I2S clock sources on MPC5200
    
    Signed-off-by: Jon Smirl <jonsmirl@gmail.com>

---
+++ sound/soc/fsl/mpc5200_psc_i2s.h
@@ -0,0 +1,13 @@
+/*
+ * Freescale MPC5200 PSC in I2S mode
+ * ALSA SoC Digital Audio Interface (DAI) driver
+ *
+ */
+ 
+#ifndef __SOUND_SOC_FSL_MPC52xx_PSC_I2S_H__
+#define __SOUND_SOC_FSL_MPC52xx_PSC_I2S_H__
+
+#define MPC52xx_CLK_INTERNAL 0
+#define MPC52xx_CLK_CELLSLAVE 1
+
+#endif /* __SOUND_SOC_FSL_MPC52xx_PSC_I2S_H__ */
