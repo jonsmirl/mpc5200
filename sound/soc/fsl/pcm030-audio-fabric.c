b8cbf5554916ea4e81fb6cd0d6192c97b19ed44b
Jon Smirl <jonsmirl@gmail.com>
2009-05-23 12:06:50 -0400

    Enable mmc-spi support

--- sound/soc/fsl/pcm030-audio-fabric.c
+++ sound/soc/fsl/pcm030-audio-fabric.c
@@ -90,6 +90,6 @@ module_exit(pcm030_fabric_exit);
 
 
 MODULE_AUTHOR("Jon Smirl <jonsmirl@gmail.com>");
-MODULE_DESCRIPTION(DRV_NAME ": mpc5200 pcm030 fabric driver");
+MODULE_DESCRIPTION("mpc5200 pcm030 fabric driver");
 MODULE_LICENSE("GPL");
 
