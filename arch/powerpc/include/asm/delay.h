a0fa7b2b0019380262a5d8d4a301c8dd0c10371d
Jon Smirl <jonsmirl@gmail.com>
2009-05-26 12:18:03 -0400

    Refresh of timur

--- arch/powerpc/include/asm/delay.h
+++ arch/powerpc/include/asm/delay.h
@@ -52,16 +52,18 @@ extern void udelay(unsigned long usecs);
  *
  * gcc will optimize out the if-statement if @delay is a constant.
  */
-#define spin_event_timeout(condition, timeout, delay, rc)                   \
-{                                                                           \
-	unsigned long __loops = tb_ticks_per_usec * timeout;                \
-	unsigned long __start = get_tbl();                                  \
-	while (!(rc = (condition)) && (tb_ticks_since(__start) <= __loops)) \
-		if (delay)                                                  \
-			udelay(delay);                                      \
-		else	                                                    \
-			cpu_relax();                                        \
-}
+#define spin_event_timeout(condition, timeout, delay)                         \
+({                                                                            \
+       typeof(condition) __ret;                                               \
+       unsigned long __loops = tb_ticks_per_usec * timeout;                   \
+       unsigned long __start = get_tbl();                                     \
+       while (!(__ret = (condition)) && (tb_ticks_since(__start) <= __loops)) \
+               if (delay)                                                     \
+                       udelay(delay);                                         \
+               else                                                           \
+                       cpu_relax();                                           \
+       __ret;                                                                 \
+})
 
 #endif /* __KERNEL__ */
 #endif /* _ASM_POWERPC_DELAY_H */
